385582fb5b9e42490940847f30890bf13566d85e
Antonio del Pardo <antonio.delpardo229161@potros.itson.edu.mx>
2023-11-20 18:01:46 -0700

    Update attendanceService.js

--- services/attendanceService.js
+++ services/attendanceService.js
@@ -4,28 +4,37 @@ import ValidationError from "../errors/ValidationError.js";
 import CourseDAO from "../DAO/courseDAO.js";
 
 export default class AttendanceService {
-    constructor(){
-        this.attendanceDAO = new AttendanceDAO();
-        this.courseDAO = new CourseDAO();
+  constructor() {
+    this.attendanceDAO = new AttendanceDAO();
+    this.courseDAO = new CourseDAO();
+  }
+
+  async registerAttendance(attendanceData) {
+    const exist = await this.attendanceDAO.getByDate(attendanceData.date);
+    if (exist)
+      throw new ValidationError(
+        "Ya se registro la asistencia para esta fecha."
+      );
+    const attendances = await this.attendanceDAO.getByCourse(
+      attendanceData.course
+    );
+
+    // Si no existen asistencias de ese curso, se agregan los estudiantes al curso
+    if (!attendances) {
+      const course = await this.courseDAO.getById(attendanceData.course);
+      course.students = attendanceData.students.map(
+        (student) => student.student
+      );
     }
 
-    async registerAttendance(attendanceData){
-        const exist = await this.attendanceDAO.getByDate(attendanceData.date);
-        if(exist) throw new ValidationError("Ya se registro la asistencia para esta fecha.");
-        const attendances = await this.attendanceDAO.getByCourse(attendanceData.course);
+    const attendance = await this.attendanceDAO.create(attendanceData);
+    return attendance;
+  }
 
-        // Si no existen asistencias de ese curso, se agregan los estudiantes al curso
-        if(!attendances){
-            const course = await this.courseDAO.getById(attendanceData.course);
-            course.students = attendanceData.students.map(student => student.student);
-        }
-
-        const attendance = await this.attendanceDAO.create(attendanceData);
-        return attendance;
-    }
-
-    async getAttendanceByCourse(courseId){
-        const attendance = await this.attendanceDAO.getByCourse(courseId);
-        return attendance;
-    }
+  async getAttendancesByCourse(courseId) {
+    const attendance = await this.attendanceDAO.getAttendancesByCourse(
+      courseId
+    );
+    return attendance;
+  }
 }
